bff8566a0560bd727f83d9a67c92a6fe46db33f5
kintwadi <joinemabi@gmail.com>
2025-11-29 16:03:26 +0100

    no translation

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/frei-kirche-base/',
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  base: mode === 'production' ? '/frei-kirche-base/' : '/',
+}))
